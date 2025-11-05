a63330ecf6921a69968172e9610a4a634ab3f19f
Thara <84392600+tharakeswararao@users.noreply.github.com>
2025-11-05 13:10:31 +0530

    feat: add v9 endpoint

--- src/main/java/dev/gms2/workflowtest/controller/HelloWorldController.java
+++ src/main/java/dev/gms2/workflowtest/controller/HelloWorldController.java
@@ -35,4 +35,9 @@ public class HelloWorldController {
         return "v8 - Hello workflowtest";
     }
 
+    @GetMapping("/hello/v9")
+    public String helloV9() {
+        return "v9 - Hello workflowtest";
+    }
+
 }
