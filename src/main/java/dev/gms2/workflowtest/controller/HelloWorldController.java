421fc32090be8cfab4fd7d9291db21159a2a3a2b
Thara <84392600+tharakeswararao@users.noreply.github.com>
2025-11-05 10:50:13 +0530

    feat: updated the message in controller

--- src/main/java/dev/gms2/workflowtest/controller/HelloWorldController.java
+++ src/main/java/dev/gms2/workflowtest/controller/HelloWorldController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World - Spring Boot";
+        return "Hello World - Spring Boot | workflowtest :) :)";
     }
 }
